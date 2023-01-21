cbac300dbd46e2b760c4d845eea61565729897d4
alexislopezdev <alexis.lopez201863@yahoo.com>
2023-01-21 12:25:10 -0300

    add sections

---
+++ hamburguer.js
@@ -0,0 +1,15 @@
+const hamburguer = document.getElementById("navbar__link");
+const hamburguer2 = document.getElementById("navbar__links");
+const navbarHamburguer = document.getElementById("close__hamburguer");
+const closeNavbar = document.getElementById("close__logo");
+
+hamburguer.addEventListener("click", () => {
+  hamburguer2.style.display = "block";
+  navbarHamburguer.style.display = "none";
+  closeNavbar.style.display = "block";
+});
+
+closeNavbar.addEventListener("click", () => {
+  console.log(hamburguer2);
+  hamburguer2.style.display = "none";
+});
--- index.js
+++ index.js
@@ -2,8 +2,24 @@ fetch("http://localhost:1337/api/categories")
   .then((responce) => responce.json())
   .then((data) => {
     let name = data.data[0].attributes.name;
+    
 
     let products = document.getElementById("date");
 
     products.innerHTML = `${name}`;
   });
+
+
+
+  function wrapping(gifts) {
+    let papel = "";
+    for (let i = 0; i < gifts.length; i++) {
+      papel +=  gifts[i] + "<br>";
+    }
+    return papel;
+  }
+  
+  const gifts = ["cat", "game", "socks"];
+  const wrapped = wrapping(gifts);
+  
+  console.log(wrapped);
