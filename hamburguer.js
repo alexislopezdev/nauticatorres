db44e382940ada27c989803833d9836d7fad4acb
alexislopezdev <alexis.lopez201863@yahoo.com>
2023-01-31 17:35:01 -0300

    add close navbar

--- hamburguer.js
+++ hamburguer.js
@@ -1,27 +1,21 @@
-const hamburguer = document.getElementById("navbar__link");
-const hamburguer2 = document.getElementById("navbar__links");
-const navbarHamburguer = document.getElementById("close__hamburguer");
-const closeNavbar = document.getElementById("close__logo");
+const hamburguer = document.getElementById("#navbar__link");
+const navbarHamburguer = document.getElementById("#close__hamburguer");
+const closeNavbar = document.getElementById("#close__logo");
+const hamburguer2 = document.getElementById("#navbar__links");
 
 hamburguer.addEventListener("click", () => {
-  hamburguer2.style.display = "block";
-  navbarHamburguer.style.display = "none";
-  closeNavbar.style.display = "block";
+  navbarHamburguer.style.display ="none";
+  closeNavbar.style.display ="block";
+  hamburguer2.style.display="block";
+  hamburguer.style.display="none";
 });
+
 
 closeNavbar.addEventListener("click", () => {
-  console.log("hola");
-  hamburguer2.style.display = "none";
-  
+  hamburguer2.style.display="none";
+  closeNavbar.style.display ="none";
+  navbarHamburguer.style.display ="block";
+  hamburguer.style.display="block";
   
 });
 
-
-const filterLancha = document.getElementById("#check__lancha");
-const filterId = document.getElementById("#card__id");
-
-filterLancha.addEventListener("click",() => {
-  filterId.style.display = "none";
-
-  console.log("Hola",filterId);
-});
